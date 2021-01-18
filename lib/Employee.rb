e9dfba45957a6538ae7ef09745e5bb9350a7ef2d
ccchippp <clempke90@gmail.com>
2021-01-18 16:31:23 -0500

    employees work

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,26 @@
+require 'pry'
+require './Manager.rb'
 class Employee
+   attr_accessor :name, :salary, :manager_name
+    @@all = []
+   def initialize(name, salary, manager_name)
+        @name = name
+        @salary = salary.to_i
+        @manager_name = manager_name
 
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+
+    # def paid_over(num)
+    #     @@all.map do |name|
+    #         if name[:salary] > num
+    #             p name
+    #         end
+    #     end
+    # end
 end
+pam = Employee.new("Pam Beasley", "30,000", "Michael Scott")
+jim = Employee.new("Jim Halpert", "36,000", "Michael Scott")
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,31 @@
-class Manager
+require 'pry'
+require './Employee.rb'
 
+class Manager
+    attr_accessor :name, :department, :age, :employees
+    @@all = []
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age.to_i
+
+        @@all << self
+        employees = []
+    end
+    def self.all
+        @@all
+    end  
+    def employees
+        Employee.all.select { 
+            |employee| employee.manager_name == self.name
+        }
+    end
+    def hire_employee(name, salary)
+        @employees.push(self)
+    end
+ 
 end
+michael = Manager.new("Michael Scott", "Branch", "46")
+jan = Manager.new("Jan Levinson", "Regional", "43")
+
+binding.pry
