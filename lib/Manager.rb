f72d8171a26935ea2c1b58c9818af3cc13974168
ccchippp <clempke90@gmail.com>
2021-01-19 20:53:55 -0500

    commit tree 0bcc1046821399c68e1c27ea44836eec3685d44e

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -4,24 +4,29 @@ require './Employee.rb'
 class Manager
     attr_accessor :name, :department, :age, :employees
     @@all = []
+    @@departments =[]
     def initialize(name, department, age)
         @name = name
         @department = department
         @age = age.to_i
 
         @@all << self
-        employees = []
+        @employees = []
+        @@departments << department
     end
     def self.all
         @@all
     end  
+    def departments
+        @@departments
+    end
     def employees
         Employee.all.select { 
             |employee| employee.manager_name == self.name
         }
     end
     def hire_employee(name, salary)
-        @employees.push(self)
+        @employees << Employee.new(name, salary, self.name)
     end
  
 end
